f7b07538bfa83a8973cdda3599eb2053b7db573b
Laurence Dorman <ldorman@letudiant.fr>
2016-03-17 14:34:31 +0100

    update: chooseable has similar functionality to before

--- src/block_mixins/controllable.js
+++ src/block_mixins/controllable.js
@@ -12,28 +12,34 @@ module.exports = {
   initializeControllable: function() {
     utils.log("Adding controllable to block " + this.blockID);
     this.control_ui = Dom.createElement('div', {'class': 'st-block__control-ui'});
-    Object.keys(this.controls).forEach(
-      function(cmd) {
+
+    Object.keys(this.controls).forEach(cmd => {
+
+      if (typeof this.controls[cmd] === 'function') {
         // Bind configured handler to current block context
+        this.control_ui.appendChild(this.getControlTemplate(cmd));
+
         this.addUiControl(cmd, this.controls[cmd].bind(this));
-      },
-      this
-    );
+      }
+      else {
+        this.control_ui.appendChild(this.controls[cmd].elem());
+
+        this.addUiControl(cmd, this.controls[cmd].cb, this.controls[cmd].event)
+      }
+
+    });
+
     this.inner.appendChild(this.control_ui);
   },
 
   getControlTemplate: function(cmd) {
     return Dom.createElement("a", {
-      'data-icon': cmd,
       'class': 'st-icon st-block-control-ui-btn st-block-control-ui-btn--' + cmd,
-      'html': `<svg role="img" class="st-icon">
-                  <use xlink:href="${config.defaults.iconUrl}#icon-${cmd}"/>
-                </svg>`
+      'html': `<svg role="img" class="st-icon"><use xlink:href="${config.defaults.iconUrl}#icon-${cmd}"/></svg>`
     });
   },
 
-  addUiControl: function(cmd, handler) {
-    this.control_ui.appendChild(this.getControlTemplate(cmd));
-    Events.delegate(this.control_ui, '.st-block-control-ui-btn--' + cmd, 'click', handler);
+  addUiControl: function(cmd, handler, eventType = 'click') {
+    Events.delegate(this.control_ui, '.st-block-control-ui-btn--' + cmd, eventType, handler);
   }
 };
