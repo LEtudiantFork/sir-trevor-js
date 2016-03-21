f5141ec4264dc9ff1d0e35af7a556e3079515344
Laurence Dorman <ldorman@letudiant.fr>
2016-03-21 14:50:33 +0100

    remove: unecessary locale (en)

--- src/locales.js
+++ src/locales.js
@@ -5,59 +5,6 @@ var config = require('./config');
 var utils = require('./utils');
 
 var Locales = {
-    en: {
-        general: {
-            'delete':                     'Delete?',
-            'drop':                         'Drag __block__ here',
-            'paste':                        'Or paste URL here',
-            'upload':                     '...or choose a file',
-            'close':                        'close',
-            'position':                 'Position',
-            'wait':                         'Please wait...',
-            'link':                         'Enter a link',
-            'yes':                            'Yes',
-            'no':                             'No'
-        },
-        errors: {
-            'title': "You have the following errors:",
-            'validation_fail': "__type__ block is invalid",
-            'block_empty': "__name__ must not be empty",
-            'type_missing': "You must have a block of type __type__",
-            'required_type_empty': "A required block type __type__ is empty",
-            'load_fail': "There was a problem loading the contents of the document"
-        },
-        blocks: {
-            text: {
-                'title': "Text"
-            },
-            list: {
-                'title': "List"
-            },
-            quote: {
-                'title': "Quote",
-                'credit_field': "Credit"
-            },
-            image: {
-                'title': "Image",
-                'upload_error': "There was a problem with your upload"
-            },
-            video: {
-                'title': "Video"
-            },
-            tweet: {
-                'title': "Tweet",
-                'fetch_error': "There was a problem fetching your tweet"
-            },
-            embedly: {
-                'title': "Embedly",
-                'fetch_error': "There was a problem fetching your embed",
-                'key_missing': "An Embedly API key must be present"
-            },
-            heading: {
-                'title': "Heading"
-            }
-        }
-    },
     fr: {
       general: {
         'delete':           'Suppression ?',
@@ -92,6 +39,15 @@ var Locales = {
         },
         media: {
           'title': "Média"
+        },
+        jcs: {
+            'title': 'JCS'
+        },
+        chart: {
+            'title': 'Chart'
+        },
+        table: {
+            'title': 'Table'
         },
         image: {
           'title': "Image",
