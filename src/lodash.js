b6fe8fa70042e3febf753ae917acaba8f2e9c32e
Laurence Dorman <ldorman@letudiant.fr>
2016-03-23 15:33:03 +0100

    update: lodash dependencies

--- src/lodash.js
+++ src/lodash.js
@@ -1,10 +1,15 @@
 'use strict';
 
-exports.isEmpty = require('lodash.isempty');
-exports.isFunction = require('lodash.isfunction');
-exports.isObject = require('lodash.isobject');
-exports.isString = require('lodash.isstring');
+exports.camelCase   = require('lodash.camelcase');
+exports.chunk       = require('lodash.chunk');
+exports.debounce    = require('lodash.debounce');
+exports.forEach     = require('lodash.foreach');
+exports.isEmpty     = require('lodash.isempty');
+exports.isFunction  = require('lodash.isfunction');
+exports.isObject    = require('lodash.isobject');
+exports.isString    = require('lodash.isstring');
 exports.isUndefined = require('lodash.isundefined');
-exports.result = require('lodash.result');
-exports.template = require('lodash.template');
-exports.uniqueId = require('lodash.uniqueid');
+exports.result      = require('lodash.result');
+exports.template    = require('lodash.template');
+exports.uniqueId    = require('lodash.uniqueid');
+exports.sortBy      = require('lodash.sortby');
