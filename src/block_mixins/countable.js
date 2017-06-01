74ee004ca43d0e9bef131d0d8b0bc6b3514b67a7
ccaillard <cailard.clement@gmail.com>
2017-06-01 17:10:14 +0200

    style countable

---
+++ src/block_mixins/countable.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var utils = require('../utils');
+
+module.exports = {
+
+    mixinName: "Countable",
+
+    initializeCountable: function() {
+        utils.log("Adding countable to block " + this.blockID);
+
+        var span = document.createElement('span');
+        span.className = "st-count";
+
+        this.el.insertBefore(span, this.inner);
+
+        this.el.addEventListener('keyup', () => { setTimeout(() => {span.innerHTML = this.inner.querySelector('[contenteditable]').textContent.trim().length; },0); });
+
+    }
+};
